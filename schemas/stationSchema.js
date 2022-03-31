176a1aa2a982b13afb0993f00605077a1398cef6
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-04-01 01:09:44 +0300

    station with location

--- schemas/stationSchema.js
+++ schemas/stationSchema.js
@@ -6,11 +6,17 @@ export default gql`
     station(id:ID!): Station
   }
   
+  type Location {
+    coordinates: [Float]
+    type: String
+  }
+  
   type Station {
     id: ID
     Title: String
     AddressLine1: String
     StateOrProvince: String
     Postcode: String
+    Location: Location
   }
 `;
